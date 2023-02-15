9ade5d05b7444eb66375aa6b5793c80da4ee9db0
Hadi <hadirezaei2828@gmail.com>
2023-02-15 13:44:01 +0330

    added Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { ThemeProvider } from "@mui/material/styles";
+import rtlPlugin from "stylis-plugin-rtl";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+import { prefixer } from "stylis";
+import { theme } from "./theme";
+import Navbar from "./components/Navbar";
+
+const cacheRtl = createCache({
+  key: "muirtl",
+  stylisPlugins: [prefixer, rtlPlugin],
+});
 
 function App() {
   return (
-    <div className="app">
-      
-    </div>
+    <CacheProvider value={cacheRtl}>
+      <ThemeProvider theme={theme}>
+        <Navbar />
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,110 @@
+import {
+  AppBar,
+  Avatar,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Toolbar,
+} from "@mui/material";
+import image01 from "../images/image-avatar.png";
+import logo from "../images/logo.png";
+import cartIcon from "../images/icon-cart.svg";
+import menuIcon from "../images/icon-menu.svg";
+import { grey } from "@mui/material/colors";
+
+const Navbar = () => {
+  const pages = ["کالکشن", "مردانه", "زنانه", "تماس با ما", "درباره ما"];
+
+  return (
+    <AppBar
+      position="sticky"
+      sx={{
+        backgroundColor: "hsl(0, 0%, 100%)",
+        height: 100,
+        boxShadow: "none",
+      }}
+    >
+      <Container
+        disableGutters
+        maxWidth="lg"
+        sx={{
+          width: "90vw",
+          borderBottom: `1px solid ${grey[200]}`,
+          py: 2,
+        }}
+      >
+        <Toolbar
+          disableGutters
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Box sx={{ display: { xs: "flex", sm: "none" }, mr: 2 }}>
+              <IconButton>
+                <img alt="menu" src={menuIcon} />
+              </IconButton>
+            </Box>
+            <Box
+              component="img"
+              sx={{
+                height: { lg: 60, md: 55, sm: 45, xs: 40 },
+                cursor: "pointer",
+              }}
+              alt="Logo"
+              src={logo}
+            />
+            <Box ml={4} sx={{ display: { xs: "none", sm: "flex" } }}>
+              {pages.map((page, index) => (
+                <Button
+                  key={index}
+                  sx={{ color: grey[600], borderRadius: "10px" }}
+                >
+                  {page}
+                </Button>
+              ))}
+            </Box>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Box
+              component="img"
+              sx={{
+                cursor: "pointer",
+                height: { lg: 25, md: 25, sm: 20, xs: 20 },
+              }}
+              alt="Logo"
+              src={cartIcon}
+              mr="2vw"
+            />
+            <Avatar
+              alt="Avatar"
+              src={image01}
+              sx={{
+                cursor: "pointer",
+                height: { lg: 45, md: 40, sm: 35, xs: 30 },
+                width: { lg: 45, md: 40, sm: 35, xs: 30 },
+              }}
+            />
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+
+export default Navbar;
